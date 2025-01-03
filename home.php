c94082e15e7c770578b1eef7c8905bb4db9894cb
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-01-03 13:24:00 +0545

    index

--- home.php
+++ home.php
@@ -1,19 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="CSS/index.css">
 </head>
+
 <body>
     <nav class="navbar">
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">notificatio</a>
+        <div class="logo-container">
+            <h1>Sajilo Ticket System</h1>
+        </div>
+        <ul class="nav-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Notifications</a></li>
+        </ul>
     </nav>
     <div class="content">
-        <h1>Welcome to the Home Page</h1>
     </div>
 </body>
+
 </html>
